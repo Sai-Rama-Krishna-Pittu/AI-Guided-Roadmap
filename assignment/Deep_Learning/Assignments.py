ff87fa371dce90f061170ac02958c5ec9431ddd5
Jayasri <j.jayasri2695@gmail.com>
2024-06-29 16:24:07 +0530

    folders

---
+++ assignment/Deep_Learning/Assignments.py
@@ -0,0 +1,12 @@
+
+"""
+List of Assignments:
+
+submit 1,2 - By July 7th, 2024
+-------------------------------
+
+1. Implement Linear Regression from scratch using Numpy,python (No libraries should be used)
+
+2. Implement Logistic Regression from scratch using Numpy,python (No libraries should be used)
+
+"""
